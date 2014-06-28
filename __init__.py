ae9bea7071885d262d74feeef66a9b794f21541c
Edgard Pimentel <pimentelrojas@gmail.com>
2014-06-27 22:55:35 -0500

    corrigiendo init

--- __init__.py
+++ __init__.py
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-=======
 import vehicle
->>>>>>> f8a9589ef4ac1ea8f890d7a038caadf3d943f095
 import surmotors_sale_order
 import surmotors_res_partner
 import surmotors_fleet_vehicle
 import surmotors_fleet_vehicle_log_contract
 import surmotors_hr_employee
 import surmotors_fleet_vehicle_cost_tipo
-<<<<<<< HEAD
 import surmotors_bahias
 import surmotors_production
 # import surmotors_fleet_vehicle_product
-=======
-import surmotors_fleet_vehicle_cost
-import surmotors_bahias
-import surmotors_production
-#import surmotors_fleet_vehicle_product
->>>>>>> f8a9589ef4ac1ea8f890d7a038caadf3d943f095
