78891eaa1db0c790f67b17212f4b94fe659c7bff
Edgard Pimentel <pimentelrojas@gmail.com>
2014-08-05 16:43:30 -0500

    validando nombre res_partner

--- res_partner/res_partner.py
+++ res_partner/res_partner.py
@@ -1,5 +1,9 @@
 # coding= utf-8
 
+import re
+
+from tools.translate import _
+
 
 from osv import fields, osv
 
@@ -12,7 +16,25 @@ class res_partner(osv.osv):
         'res_partner_ids': fields.one2many('fleet.vehicle', 'res_partner_id', 'Vehiculos'),
     }
 
+    def validate_name(self, cr, uid, name, context=None):
+        if not (re.search('^[A-Za-z\s]+$', name)):
+            raise osv.except_osv(
+                _('Alerta'),
+                _('El campo nombre solo acepta letras')
+            )
+        return True
 
+    def create(self, cr, uid, values, context=None):
+        if 'name' in values:
+            name = values['name']
+            self.validate_name(cr, uid, name)
 
+        return super(res_partner, self).create(cr, uid, values, context=context)
+
+    def write(self, cr, uid, ids, values, context=None):
+        if 'name' in values:
+            name = values['name']
+            self.validate_name(cr, uid, name)
+        return super()
 
 res_partner()
